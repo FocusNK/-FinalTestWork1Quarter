c9ac247814236b2b3b3c9e92e1c44d3792703d78
FocusNK <skachkov_e@inbox.ru>
2022-05-09 11:48:08 +0700

    Fill programm

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = new string[] { "hello", "2", "world", "Kate", "Tom", "Alice", "As"};
+string[] group = Array.FindAll(array, arrayLegth => arrayLegth.Length <= 3);
+foreach (var arrayLegth in group)
+Console.WriteLine(arrayLegth);
